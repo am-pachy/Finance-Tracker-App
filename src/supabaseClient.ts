2f6095843d0c187334def392f764613beda3d662
am-pachy <anna.marchetto.vi@gmail.com>
2026-03-16 17:31:01 +0100

    second test

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-// Usiamo il tuo URL reale e la tua chiave reale
-const supabaseUrl = 'https://zjqgwkttdnvkfxnjumzg.supabase.co';
-const supabaseAnonKey = 'sb_publishable_hIfO1IjMRxbvnXk5H63Kdw_wIFT0Oj6';
+const supabaseUrl = "https://zjqgwkttdnvkfxnjumzg.supabase.co";
+
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqcWd3a3R0ZG52a2Z4bmp1bXpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNjQxNTUsImV4cCI6MjA4ODg0MDE1NX0.OfcI5HMSBTtDCKakCzqNtdOHx8ik8YD3om76-E0X_os";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
